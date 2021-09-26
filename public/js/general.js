918ddcabb808b64d83a13e49c2f9360ee33873fc
timiwhosa <64585862+timiwhosa@users.noreply.github.com>
2021-09-26 15:56:06 +0100

    h

--- public/js/general.js
+++ public/js/general.js
@@ -24,31 +24,6 @@ var x = setInterval(function () {
     document.getElementById("demo").innerHTML = "EXPIRED";
   }
 }, 1000);
-
-
- window.addEventListener("scroll", (e) => {
-   if (document.documentElement.scrollTop > 250) {
-     document.querySelector("nav").classList.add("active");
-   } else {
-     document.querySelector("nav").classList.remove("active");
-   }
- });
-
- var cover = document.getElementsByClassName("cover")[0];
- document
-   .getElementsByClassName("menu")[0]
-   .addEventListener("click", sidenavtoogle);
- cover.addEventListener("click", sidenavtoogle);
- function sidenavtoogle(e) {
-   var sidenav = document.getElementsByClassName("side-nav")[0];
-   if (sidenav.style.left === "0px") {
-     sidenav.style.left = "-100vw";
-     cover.style.left = "-120vw";
-   } else {
-     sidenav.style.left = 0;
-     cover.style.left = 0;
-   }
- }
  function closeenroll(e) {
    e.style.display = "none";
    var enroll = document.getElementsByClassName("enroll-div")[0];
---
+++ public/js/main.js
@@ -0,0 +1,23 @@
+window.addEventListener("scroll", (e) => {
+  if (document.documentElement.scrollTop > 250) {
+    document.querySelector("nav").classList.add("active");
+  } else {
+    document.querySelector("nav").classList.remove("active");
+  }
+});
+
+var cover = document.getElementsByClassName("cover")[0];
+document
+  .getElementsByClassName("menu")[0]
+  .addEventListener("click", sidenavtoogle);
+cover.addEventListener("click", sidenavtoogle);
+function sidenavtoogle(e) {
+  var sidenav = document.getElementsByClassName("side-nav")[0];
+  if (sidenav.style.left === "0px") {
+    sidenav.style.left = "-100vw";
+    cover.style.left = "-120vw";
+  } else {
+    sidenav.style.left = 0;
+    cover.style.left = 0;
+  }
+}
