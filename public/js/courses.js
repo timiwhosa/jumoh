300638c79c81f120ed95ad313d96f1b7f8d008b9
timiwhosa <64585862+timiwhosa@users.noreply.github.com>
2021-10-11 12:42:42 +0100

    ifotim

--- public/js/courses.js
+++ public/js/courses.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Oct 23, 2021 00:00:00").getTime();
+var countDownDate = new Date("Oct 18, 2021 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
